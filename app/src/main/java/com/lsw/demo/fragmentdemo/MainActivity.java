33eb48ae4ca7ba18ff603b5330620c2a8236ea4c
liushuwei <liushuwei@qiyi.com>
2017-10-12 18:17:43 +0800

    fragment实现

--- app/src/main/java/com/lsw/demo/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/lsw/demo/fragmentdemo/MainActivity.java
@@ -1,14 +1,45 @@
 package com.lsw.demo.fragmentdemo;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+//        replaceFragment(new RightFragment());
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                replaceFragment(new AnotherRightFragment());
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.addToBackStack(null);//加入回退栈只会销毁View组件，不会销毁fragment
+        transaction.commit();
+    }
+
+    public void test(){
+
+    }
+
 }
